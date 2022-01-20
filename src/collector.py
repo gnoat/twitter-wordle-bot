9af31f4ef5fde68b771a89d11f6ba580a755e189
metriczulu <stephenson.shane.a@gmail.com>
2022-01-19 23:59:47 -0500

    updated to allowing caching results

--- src/collector.py
+++ src/collector.py
@@ -10,6 +10,7 @@ import time
 from configparser import ConfigParser
 import pathlib
 import datetime
+import json
 
 
 # Easy way to map emojis to create graph
@@ -78,7 +79,7 @@ def process_results(result_dict=dict(), height=6, max_n=0, raw_results=None):
             color = "rs"
         else:
             color = "ys"
-        current_row = [emoji_map[row]] + normed_results[row] * [emoji_map[color]] + (height - normed_results[row]) * [emoji_map["bs"]]
+        current_row = [emoji_map[row]] + normed_results[row] * [emoji_map[color]] + (height - normed_results[row]) * [emoji_map["ws"]]
         results_matrix.append(current_row)
     results_str = "\n".join(["".join(row) for row in list(zip(*results_matrix))[::-1]]) # Take the reverse transpose of the graph and convert to multiline string
     raw_nums = reduce(lambda a, b: a + b, [v * [int(k) if (k != "X") else 7] for k, v in raw_results.items()])
@@ -112,6 +113,49 @@ def infer_wordle_num(api):
     return max(all_wordle_nums)
 
 
+def sort_times(hr_mins_str):
+    '''
+    Sort a list of datetime.time objects by their hour, then minute
+    '''
+    if hr_mins_str is None: return None
+    hr = int(hr_mins_str.split(":")[0])
+    mins = int(hr_mins_str.split(":")[1])
+    switching_time = datetime.datetime.combine(datetime.date.today(), datetime.time(hr, mins))
+    if switching_time < datetime.datetime.now():
+        switching_time += datetime.timedelta(days=1)
+    return switching_time
+
+
+def save_cache(result_dict, configs, to_cache=True, wordle_num=""):
+    '''
+    Take processed results and cache them to disk
+    '''
+    cache_file = pathlib.Path(__file__).parent / configs["settings"]["cache_file"]
+    if to_cache:
+        with open(cache_file, "w") as f:
+            f.write(json.dumps({
+                "wordle_num": wordle_num,
+                "result_dict": result_dict
+                }))
+        print("Cached results for num", wordle_num)
+        print("Cache saved to", cache_file)
+    return result_dict.copy()
+
+
+def read_cache(configs):
+    '''
+    Read cached results from disk
+    '''
+    cache_file = pathlib.Path(__file__).parent / configs["settings"]["cache_file"]
+    if cache_file.is_file():
+        print("Reading cache from", cache_file)
+        with open(cache_file, "r") as f:
+            cache = json.loads(f.read())
+        return int(cache["wordle_num"]) if (cache["wordle_num"] is not None) else None, cache["result_dict"]
+    else:
+        return None, dict()
+
+
 if __name__ == "__main__":
     # Read configs
     configs = ConfigParser()
@@ -125,30 +169,35 @@ if __name__ == "__main__":
     # Parse arguments
     parser = ArgumentParser(description="Collect Wordle results from Twitter")
     parser.add_argument(
+        "-c",
+        "--cache",
+        action="store_true",
+        help="Read cached results into memory when starting.")
+    parser.add_argument(
         "-n",
         "--num",
         type=int,
-        help="Wordle puzzle number to score"
+        help="Wordle puzzle number to score."
     )
     parser.add_argument(
         "-y",
         "--y_height",
         type=int,
         default=8,
-        help="Height of graph"
+        help="Height of graph."
     )
     parser.add_argument(
         "-w",
         "--wait",
         type=int,
         default=5,
-        help="Wait between searches in mins"
+        help="Wait between searches in mins."
     )
     parser.add_argument(
-        "-c",
-        "--continuous",
-        action="store_true",
-        help="Update every iteration")
+        "-u",
+        "--update_time",
+        type=str,
+        help="Time to send tweet to update with current scores.  If not specified, will only update at switching time.")
     parser.add_argument(
         "-m",
         "--max_wordle_num",
@@ -160,38 +209,66 @@ if __name__ == "__main__":
         type=int,
         default=150,
         help="Max number of tweets to query every wait period.")
+    parser.add_argument(
+        "-s",
+        "--switch",
+        type=str,
+        default=configs["settings"]["switch_puzzles"],
+        help="Time to switch to next Wordle puzzle num in format HH:MM.  Defaults to time in configuration if nothing is set.")
     args = parser.parse_args()
+
+    # Some initial environment setup
     results_tracker = dict()
-    hr = int(configs["settings"]["switch_days"].split(":")[0])
-    mins = int(configs["settings"]["switch_days"].split(":")[1])
-    switching_time = datetime.datetime.combine(datetime.date.today(), datetime.time(hr, mins))
     wordle_num = infer_wordle_num(api) if (args.num is None) else args.num
-    if switching_time < datetime.datetime.now():
-        switching_time += datetime.timedelta(days=1)
+    switching_time = sort_times(args.switch)
+    update_time = sort_times(args.update_time)
+    if args.cache:
+        cache_wordle_num, cache_results = read_cache(configs)
+        if (cache_wordle_num == args.num) or (args.num is None):
+            wordle_num = cache_wordle_num
+            results_tracker = cache_results
+    
     print("Starting bot...")
+    print("Wordle num:", wordle_num)
 
     while(True if (args.max_wordle_num is None) else (wordle_num > args.max_wordle_num)): # Loop until max_wordle_num is reached
         while(datetime.datetime.now() < switching_time): # Loop until switch time from configs has passed
+
             print("Current time:", datetime.datetime.now())
             print("Ending time:", switching_time)
+
             updated_tracker = pull_results(api, configs, wordle_num=wordle_num, result_dict=results_tracker, count=150) # pull and update results
             top_msg, additional_msg = create_messages(updated_tracker, height=args.y_height, wordle_num=wordle_num) # create messages from results
             print(top_msg)
             print(additional_msg)
-            results_tracker = updated_tracker.copy() # update results tracker to include new results
-            if args.continuous:
-                print("Sending Tweets Continuous!")
+            results_tracker = save_cache(results_tracker, configs, to_cache=args.cache, wordle_num=wordle_num) # update results tracker to include new results
+            
+            if (args.update_time is not None) and (update_time < datetime.datetime.now()): # update time if specified:
+
+                print("Sending Tweet Update!")
+
                 initial_response = api.update_status(status=top_msg)
                 api.update_status(status=f"{configs['account']['name']}\n" + additional_msg, in_reply_to_status_id=initial_response.id)
+                update_time += datetime.timedelta(days=1)
+
             time.sleep(60 * args.wait)
 
         try:
+
             print("Sending Tweets!")
+
             initial_response = api.update_status(status=top_msg)
             api.update_status(status=f"{configs['account']['name']}\n" + additional_msg, in_reply_to_status_id=initial_response.id)
-        except Exception as e:
-                print("Exception:", e)
+            
+            # Once switch time has passed, increment wordle_num and switch time before restarting tweet collection
+            results_tracker = dict()
+            wordle_num += 1
+            switching_time += datetime.timedelta(days=1)
 
-        # Once switch time has passed, increment wordle_num and switch time before restarting tweet collection
-        wordle_num += 1
-        switching_time += datetime.timedelta(days=1)
+            print("Results tracker reset!")
+            print("Wordle num:", wordle_num)
+            print("Switching time:", switching_time)
+
+        except Exception as e:
+
+                print("Exception:", e)
--- test/test_collector.py
+++ test/test_collector.py
@@ -6,12 +6,12 @@ from collections import Counter
 
 
 def test_process_results(get_results):
-    expected_graph = '''⬛⬛⬛🟩⬛⬛⬛
-⬛⬛⬛🟩🟨⬛⬛
-⬛⬛🟨🟩🟨⬛⬛
-⬛⬛🟨🟩🟨⬛⬛
-⬛⬛🟨🟩🟨🟨⬛
-⬛🟨🟨🟩🟨🟨🟥
+    expected_graph = '''⬜⬜⬜🟩⬜⬜⬜
+⬜⬜⬜🟩🟨⬜⬜
+⬜⬜🟨🟩🟨⬜⬜
+⬜⬜🟨🟩🟨⬜⬜
+⬜⬜🟨🟩🟨🟨⬜
+⬜🟨🟨🟩🟨🟨🟥
 1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣❌'''
 
     raw_results, graph, med, avg, std = collector.process_results(get_results)
@@ -20,3 +20,4 @@ def test_process_results(get_results):
     assert std == 1.27
     assert raw_results == Counter({'4': 58, '5': 46, '3': 37, '6': 24, '2': 14, 'X': 6, '1': 2})
     assert graph == expected_graph
+    print(graph)
