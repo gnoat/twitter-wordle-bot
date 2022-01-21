c9f8bb8ae40e2ce26deb9d60eb613ce6482e6765
metriczulu <stephenson.shane.a@gmail.com>
2022-01-21 09:10:24 -0500

    reformatted

--- test/conftest.py
+++ test/conftest.py
@@ -6,7 +6,6 @@ import json
 
 
 class TweetQuery:
-
     def __init__(self, user_id, text):
         self.text = text
         self.user = lambda s: s
@@ -37,12 +36,17 @@ def get_results():
 @pytest.fixture
 def mock_twitter_api():
     class MockTwitterAPI:
-
         def __init__(self, *args, **kwargs):
             pass
 
         def search_tweets(*args, **kwargs):
-            with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), f"resources/mock_query.json"), "r") as f:
-                return [TweetQuery(d['user']['id'], d['text']) for d in json.load(f)]
+            with open(
+                os.path.join(
+                    os.path.dirname(os.path.realpath(__file__)),
+                    f"resources/mock_query.json",
+                ),
+                "r",
+            ) as f:
+                return [TweetQuery(d["user"]["id"], d["text"]) for d in json.load(f)]
 
     return MockTwitterAPI()
